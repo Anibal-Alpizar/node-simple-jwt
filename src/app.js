64a30c040eefb657fcf03812ddf52e4c14b5d54a
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-03 20:27:55 -0600

    using auth route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import auth from './controllers/auth.controller.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // server understand data from form and convert it to javascript object
+
+app.use('/', auth);
 
 export default app;
