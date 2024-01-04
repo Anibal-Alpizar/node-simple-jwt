9bf379fa9c46a968ab3bdc39fdde2ea7fb8f2bed
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-03 23:17:08 -0600

    encrypt password method

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,16 @@
 import { Schema, model } from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new Schema({
     userName: String,
     email: String,
     password: String
 })
+
+userSchema.statics.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10) // 10 is the number of rounds
+    return bcrypt.hash(password, salt) // hash the password with the salt
+   
+}
 
 export default model('User', userSchema)
